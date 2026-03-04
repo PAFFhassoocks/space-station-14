2ee937a9cb4e621a20211f07d055631600e4c3f4
Samuel-C <samuelcamilo8@gmail.com>
2026-03-04 11:39:32 -0300

    toggle armor comp

---
+++ Content.Shared/Item/ItemToggle/Components/ItemToggleArmorComponent.cs
@@ -0,0 +1,20 @@
+﻿using Content.Shared.Damage;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Item.ItemToggle.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ItemToggleArmorComponent : Component
+{
+    /// <summary>
+    /// The damage reduction when activated
+    /// </summary>
+    [DataField]
+    public DamageModifierSet? ActivatedModifiers;
+
+    /// <summary>
+    /// The damage reduction when deactivated
+    /// </summary>
+    [DataField]
+    public DamageModifierSet? DeactivatedModifiers;
+}
